cb9b45cc7c9d8300ae17f7276e9701444660289a
Matheus Sousa <matheusss.dev01@gmail.com>
2025-05-24 16:23:56 -0300

    Limpando o projeto

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
 #include <Arduino.h>
 
 void setup() {
-    Serial.begin(19200);
-    pinMode(LED_BUILTIN, OUTPUT);
+    
 }
 
 void loop() {
-
-    int estado_led = 0;
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-
-    estado_led = digitalRead(LED_BUILTIN);
-    Serial.println(estado_led);
-
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-
-    estado_led = digitalRead(LED_BUILTIN);
-    Serial.println(estado_led);
     
 }
