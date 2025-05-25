f2b533e51bede812b16af2cb1bd236dd3d9779ca
Matheus Sousa <matheusss.dev01@gmail.com>
2025-05-24 23:15:53 -0300

    Usando StringBuilder e Capturando UID

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,50 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-// Pinos conforme a imagem
-#define SS_PIN GPIO_NUM_4  // SDA
-#define RST_PIN GPIO_NUM_2 // RST
+#define SS_PIN GPIO_NUM_4
+#define RST_PIN GPIO_NUM_2
+#define MONITOR_SPEED 115200
+#define CARD_READ_DELAY 1000
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Criar instância do objeto MFRC522
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+String dump_byte_array(byte *buffer, byte bufferSize);
 
 void setup()
 {
-    Serial.begin(115200);
-    SPI.begin();        // Iniciar barramento SPI
-    mfrc522.PCD_Init(); // Iniciar MFRC522
+    Serial.begin(MONITOR_SPEED);
+    SPI.begin();
+    mfrc522.PCD_Init();
+    Serial.print(F("Reader "));
+    Serial.print(F(": "));
+    mfrc522.PCD_DumpVersionToSerial();
     Serial.println("Aproxime um cartão RFID do leitor...");
 }
 
 void loop()
 {
-    // Verifica se há nova tag presente
-    if (!mfrc522.PICC_IsNewCardPresent())
+
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
     {
-        return;
+        String uid = dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+        Serial.println(uid);
+        delay(CARD_READ_DELAY);
+    }
+}
+
+String dump_byte_array(byte *buffer, byte bufferSize)
+{
+    String uidStr = "";
+
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        if (buffer[i] < 0x10)
+            uidStr += "0";
+        uidStr += String(buffer[i], HEX);
+        if (i < bufferSize - 1)
+            uidStr += ":";
     }
 
-    // Verifica se consegue ler o conteúdo
-    if (!mfrc522.PICC_ReadCardSerial())
-    {
-        return;
-    }
-
-    // Mostrar UID no Serial Monitor
-    Serial.print("UID da tag: ");
-    for (byte i = 0; i < mfrc522.uid.size; i++)
-    {
-        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-    }
-    Serial.println();
-
-    delay(1000); // Aguarda 1 segundo antes de continuar
+    uidStr.toUpperCase();
+    return uidStr;
 }
