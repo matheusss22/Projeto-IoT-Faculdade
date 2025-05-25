721daca21c67de3aad048d42fbd8743a2f7fd569
Matheus Sousa <matheusss.dev01@gmail.com>
2025-05-24 21:52:04 -0300

    Leitura RFID validada

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,41 @@
 #include <MFRC522.h>
 
 // Pinos conforme a imagem
-#define SS_PIN 4   // SDA
-#define RST_PIN 2  // RST
+#define SS_PIN GPIO_NUM_4  // SDA
+#define RST_PIN GPIO_NUM_2 // RST
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Criar instância do objeto MFRC522
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Criar instância do objeto MFRC522
 
-void setup() {
-  Serial.begin(115200);
-  SPI.begin();               // Iniciar barramento SPI
-  mfrc522.PCD_Init();        // Iniciar MFRC522
-  Serial.println("Aproxime um cartão RFID do leitor...");
+void setup()
+{
+    Serial.begin(115200);
+    SPI.begin();        // Iniciar barramento SPI
+    mfrc522.PCD_Init(); // Iniciar MFRC522
+    Serial.println("Aproxime um cartão RFID do leitor...");
 }
 
-void loop() {
-  // Verifica se há nova tag presente
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
+void loop()
+{
+    // Verifica se há nova tag presente
+    if (!mfrc522.PICC_IsNewCardPresent())
+    {
+        return;
+    }
 
-  // Verifica se consegue ler o conteúdo
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+    // Verifica se consegue ler o conteúdo
+    if (!mfrc522.PICC_ReadCardSerial())
+    {
+        return;
+    }
 
-  // Mostrar UID no Serial Monitor
-  Serial.print("UID da tag: ");
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-  }
-  Serial.println();
+    // Mostrar UID no Serial Monitor
+    Serial.print("UID da tag: ");
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(mfrc522.uid.uidByte[i], HEX);
+    }
+    Serial.println();
 
-  delay(1000);  // Aguarda 1 segundo antes de continuar
+    delay(1000); // Aguarda 1 segundo antes de continuar
 }
