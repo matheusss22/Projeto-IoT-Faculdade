ae7eb5193bd85e2886a62fdca65b5273f2d6695e
Matheus Sousa <matheusss.dev01@gmail.com>
2025-05-28 19:25:11 -0300

    Lógica para ler cartão autorizado - Matheus

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,30 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define DEBUG true
+#define MONITOR_SPEED 115200
 #define SS_PIN GPIO_NUM_4
 #define RST_PIN GPIO_NUM_2
-#define MONITOR_SPEED 115200
 #define CARD_READ_DELAY 1000
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-String dump_byte_array(byte *buffer, byte bufferSize);
+
+void dump_byte_array(char *uidStr, byte *buffer, byte bufferSize);
 
 void setup()
 {
     Serial.begin(MONITOR_SPEED);
     SPI.begin();
     mfrc522.PCD_Init();
+
+    #if DEBUG
+    Serial.println("DEBUG ON");
     Serial.print(F("Reader "));
     Serial.print(F(": "));
     mfrc522.PCD_DumpVersionToSerial();
-    Serial.println("Aproxime um cartão RFID do leitor...");
+    Serial.println("Approach an RFID card to the reader...");
+    #endif
 }
 
 void loop()
@@ -27,25 +33,34 @@ void loop()
 
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
     {
-        String uid = dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-        Serial.println(uid);
+        #if DEBUG
+        char uidStr[40] = "";
+        dump_byte_array(uidStr, mfrc522.uid.uidByte, mfrc522.uid.size);
+        Serial.println(uidStr);
         delay(CARD_READ_DELAY);
+
+        if (strcmp(uidStr, "53:B2:61:0B") == 0) {
+            Serial.println("Cartão autorizado!");
+        } else {
+            Serial.println("Cartão negado!");
+        }
+        #endif
     }
 }
 
-String dump_byte_array(byte *buffer, byte bufferSize)
-{
-    String uidStr = "";
+void dump_byte_array(char *uidStr, byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        
+        if (i > 0) {
+            strcat(uidStr, ":");
+        }
 
-    for (byte i = 0; i < bufferSize; i++)
-    {
-        if (buffer[i] < 0x10)
-            uidStr += "0";
-        uidStr += String(buffer[i], HEX);
-        if (i < bufferSize - 1)
-            uidStr += ":";
+        char hexByte[4];
+        sprintf(hexByte, "%02X", buffer[i]);
+        strcat(uidStr, hexByte);
     }
-
-    uidStr.toUpperCase();
-    return uidStr;
 }
+
+
+
+
